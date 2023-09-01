c08aba1777fa505b912ff4de9a2a8b74330e7af1
myron <m.saveliev.lionwood@gmail.com>
2023-09-01 17:43:38 +0300

    deleted

--- persons-list/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp, h } from "vue";
-import wrapper from "vue3-webcomponent-wrapper";
-
-import App from "./App.vue";
-
-const CustomElement = wrapper(App, createApp, h);
-
-window.customElements.define("data-table", CustomElement);
